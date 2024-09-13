eda76b209c49f6360163b245804a15e9a2bc4aa3
AnanyaShukla029 <98550710+AnanyaShukla029@users.noreply.github.com>
2024-09-13 22:10:30 +0530

    Prepare for deployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Pricing from './Components/Pricing/Pricing';
+import Reviews from './Components/Review/Review';
+import Login from './Components/LoginSignup/Login'; 
+import Signup from './Components/LoginSignup/Signup';  
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />  
+        <Route path="/signup" element={<Signup />} /> 
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/reviews" element={<Reviews />} />
+      </Routes>
+    </Router>
   );
 }
 
